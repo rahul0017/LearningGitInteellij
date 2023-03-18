88b45208c5b28d17dfbe50b6af09183b1aae5b5f
rahulrao0017 <198321@ust-global.com>
2023-03-18 14:35:31 +0530

    commit 3

--- src/main/java/com/git/learning/learngit/LearngitApplication.java
+++ src/main/java/com/git/learning/learngit/LearngitApplication.java
@@ -9,6 +9,8 @@ public class LearngitApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(LearngitApplication.class, args);
 		System.out.println("test the gitk sir");
+		System.out.println("im in featur ");
+		//master change needs to come in feature
 	}
 
 }
