f6190f919d02e9922e0d4a3390ec3646ad1079ad
rahulrao0017 <198321@ust-global.com>
2023-03-18 12:13:45 +0530

    initial commit

---
+++ src/test/java/com/git/learning/learngit/LearngitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.git.learning.learngit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearngitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
