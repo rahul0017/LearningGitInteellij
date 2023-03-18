14c81fcc2edf41088e04c8ae3c576e73f6b1aa39
rahulrao0017 <198321@ust-global.com>
2023-03-18 12:46:05 +0530

    initial commit 2

---
+++ src/main/java/com/git/learning/learngit/LearngitApplication.java
@@ -0,0 +1,14 @@
+package com.git.learning.learngit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearngitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearngitApplication.class, args);
+		System.out.println("test the gitk sir");
+	}
+
+}
